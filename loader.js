12d508a401b97c94d34e47d82fd8d5d8db41c63a
zaidimarkaz <zaidimarkaz@gmail.com>
2025-12-10 15:50:06 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loader = document.getElementById("loaderScreen");
+
+  // Montre le loader immédiatement
+  loader.style.opacity = "1";
+  loader.style.display = "flex";
+
+  // Force d'afficher le loader pendant 2 secondes
+  setTimeout(() => {
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500); // temps de fade-out
+  }, 20000); // durée obligatoire d'affichage
+});
